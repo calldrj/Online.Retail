57a15f106b7bb4921457bec3866a3e7677985dc7
Tuan Nguyen <efranchisor@gmail.com>
2021-04-27 00:03:30 -0700

    backend

---
+++ login.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import Error
+
+# Fill in the database you want to work on
+working_database = 'Retail'
+
+# Fill in your mySQL root pass
+my_password = '*****'
+
+def validateLogin(username, password):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database=working_database,
+                                             user='root',
+                                             password=my_password)
+        if connection.is_connected():
+            cursor = connection.cursor(buffered=True)
+            cursor.execute('select database();')
+        
+            """ Queries to find Buyer ID and the Cart associated with the buyer """
+            q = "SELECT buyer_ID FROM buyers \
+                WHERE login_ID = '{}' AND passw = '{}'".\
+                    format(username.get(), password.get())
+            cursor.execute(q)
+            if cursor.rowcount == 1:
+                buyer_id = cursor.fetchall()[0][0]
+                print('Buyer ID found = ', buyer_id)
+                q = "SELECT item_ID FROM cart \
+                     WHERE buyer_ID = '{}'".format(buyer_id)
+                cursor.execute(q)
+                item_IDs = [x[0] for x in cursor.fetchall()]
+                print('Item ID found in Cart = ', item_IDs)  
+                # Cart[buyer_id] = item_IDs
+            else:
+                print('Buyer ID  not found!')
+            
+    except Error as e:
+        print('Error while connecting to MySQL', e)
+
+    finally:
+        if (connection.is_connected()):
+            connection.commit()
+            cursor.close()
+            connection.close()
+            
+    return
